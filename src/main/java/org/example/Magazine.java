e7fc15a5fb24f4c272160024e337b26acd091211
Ivan <ivan@MacBook.N4>
2023-05-27 08:57:21 +0300

    new users class

--- src/main/java/org/example/Magazine.java
+++ src/main/java/org/example/Magazine.java
@@ -15,21 +15,8 @@ public class Magazine implements Printable{
             num.print();
         }
 
-
-
-
-
     }
-
-//    public static void printMagazines (String[] myArray) {
-//        for (int i=0; i< myArray.length; i++) {
-//            System.out.println(myArray[i]);
-//        }
-//    }
-
-
-
-    }
+}
 
 
 
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class User implements Printable {
+
+    String firstName, secondName;
+    Integer age;
+
+    @Override
+    public String toString() {
+        return firstName + " "+ secondName +" Age: " + age;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(firstName +" " + secondName + age);
+    }
+}
