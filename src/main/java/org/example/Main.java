42e252aea60f9d75ebe66c7b136f9775ebd5013e
Ivan <ivan@MacBook.N4>
2023-06-23 12:05:49 +0300

    code cleanup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,36 +16,12 @@ public class Main {
 
         Random random = new Random();
 
-//        Book firstBook = new Book();
-//        Book secondBook = new Book();
-//        Magazine firstMagazine = new Magazine();
-//        Magazine secondMagazine = new Magazine();
-//
-//        firstMagazine.magazineName = "BurdaModa";
-//        firstBook.bookName = "Harry Potter";
-//        secondMagazine.magazineName = "Life adn since";
-//        secondBook.bookName = "Idiot";
-//
-//        //System.out.println(firstMagazine.magazineName);
-//
-//        Printable [] myArray = new Printable[4];
-//        myArray[0] = firstMagazine;
-//        myArray[1] = firstBook;
-//        myArray[2] = secondMagazine;
-//        myArray[3] = secondBook;
-//
-////        for (int i=0; i<2 ; i++){
-////            myArray[i].print();
-////        }
-//
-//        //Magazine.printMagazines(myArray);
-//        Book.printBook(myArray);
 
         ArrayList<User> users = new ArrayList();
 
-        int midAge = 0;
-        int numberOfUsersStartsFromAorS = 0;
-        int under18 = 0;
+        AtomicInteger midAge = new AtomicInteger();
+        AtomicInteger numberOfUsersStartsFromAorS = new AtomicInteger();
+        AtomicInteger under18 = new AtomicInteger();
 
         for (int i = 0; i < 10; i++) {
 
@@ -61,24 +38,36 @@ public class Main {
             Collections.sort(users, ((o1, o2) -> o1.age - o2.age));
 
 
-            for (int i = 0; i < users.size(); i++) {
+            users.forEach(user -> {
 
-                User curUser = users.get(i);
-
-                midAge = midAge + curUser.age;
-
-                if (curUser.age < 18) {
-
-                    under18++;
+                midAge.set(midAge.get() + user.age);
+                if (user.age < 18) {
+                    under18.getAndIncrement();
                 }
+                if (user.secondName.toUpperCase().startsWith("S") || user.secondName.toUpperCase().startsWith("A")) {
 
-                if (curUser.secondName.toUpperCase().startsWith("S") || curUser.secondName.toUpperCase().startsWith("A")) {
-
-                    numberOfUsersStartsFromAorS++;
+                    numberOfUsersStartsFromAorS.getAndIncrement();
                 }
-            }
+            } );
+
+//            for (int i = 0; i < users.size(); i++) {
+//
+//                User curUser = users.get(i);
+//
+//                midAge = midAge + curUser.age;
+//
+//                if (curUser.age < 18) {
+//
+//                    under18++;
+//                }
+//
+//                if (curUser.secondName.toUpperCase().startsWith("S") || curUser.secondName.toUpperCase().startsWith("A")) {
+//
+//                    numberOfUsersStartsFromAorS++;
+//                }
+//            }
             users.forEach(System.out::println);
-            System.out.println("Mid age of users is : " + midAge / users.size() + " Number of users younger then 18 are:" + under18 + " Number of users name starts from A or S :" + numberOfUsersStartsFromAorS);
+            System.out.println("Mid age of users is : " + midAge.get() / users.size() + " Number of users younger then 18 are: " + under18 + " Number of users name starts from A or S :" + numberOfUsersStartsFromAorS);
 
             users.forEach(user -> user.print());
 
